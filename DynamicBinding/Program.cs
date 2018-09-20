542774715537e6061ae3439f6b75f4b4712c5038
yeasin habib <yeasinhabib1994@gmail.com>
2018-09-20 15:44:29 +0600

    Add project files.

---
+++ DynamicBinding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic name = "Yeasin";
+            name = 24; // changed from string type to int type.
+            int age = name;
+            Console.WriteLine("age: "+age);
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b; // c also becomes dynamic;
+            Console.WriteLine("c: "+c);
+
+            int number = 10;
+            dynamic score = number; // score becomes an int type.
+            int point = score;
+            Console.WriteLine("point: " + point);
+        }
+    }
+}
